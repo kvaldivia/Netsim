ce0d25e6f01370758e1204d54c3f660b413ffe53
Kenny <kvaldivia@yandex.com>
2016-06-09 11:41:39 -0500

    Added cucumber to toolset.

---
+++ src/test/java/netsim/Stepdefs.java
@@ -0,0 +1,9 @@
+package netsim;
+
+import cucumber.api.java.en.Given;
+
+public class Stepdefs {
+    @Given("^An access point has been created.$")
+    public void accessPointCreation() throws Throwable {
+    }
+}
